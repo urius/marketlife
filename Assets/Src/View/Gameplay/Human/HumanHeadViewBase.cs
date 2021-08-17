ae77c3179a6369a5ad4bf066c9db92598f496800
Urius <isup89@rambler.ru>
2021-08-17 12:17:13 +0300

    fix face animations

--- Assets/Src/View/Gameplay/Human/HumanHeadViewBase.cs
+++ Assets/Src/View/Gameplay/Human/HumanHeadViewBase.cs
@@ -2,12 +2,7 @@ using UnityEngine;
 
 public abstract class HumanHeadViewBase : MonoBehaviour
 {
-    protected SpritesProvider SpritesProvider;
-
-    virtual protected void Awake()
-    {
-        SpritesProvider = SpritesProvider.Instance;
-    }
+    protected SpritesProvider SpritesProvider => SpritesProvider.Instance;
 
     public abstract void SetHair(int hairId);
 }

5f4f9a00c9d13e950bef38c2d4ace8913247edb2
Urius <isup89@rambler.ru>
2021-09-23 12:52:31 +0300

    Take DailyBonus command minor update + plan update

--- Assets/Src/Commands/HandleTakeDailyBonusCommand.cs
+++ Assets/Src/Commands/HandleTakeDailyBonusCommand.cs
@@ -7,46 +7,50 @@ public struct HandleTakeDailyBonusCommand
     {
         var playerModel = PlayerModelHolder.Instance.UserModel;
         var gameStateModel = GameStateModel.Instance;
-        var dailyBonusPopupViewModel = gameStateModel.ShowingPopupModel as DailyBonusPopupViewModel;
         var bonusConfig = GameConfigManager.Instance.DailyBonusConfig;
         var dispatcher = Dispatcher.Instance;
         var screenCalculator = ScreenCalculator.Instance;
 
-        var bonusConfigItems = bonusConfig.DailyBonusConfig;
-        for (var i = 0; i < bonusConfigItems.Length; i++)
-        {
-            var bonusConfigItem = bonusConfigItems[i];
-            if (dailyBonusPopupViewModel.CurrentBonusDay >= bonusConfigItem.DayNum)
-            {
-                var screenPoint = screenCalculator.WorldToScreenPoint(itemsWorldPositions[i]);
-                if (bonusConfigItem.Reward.IsGold)
-                {
-                    dispatcher.UIRequestAddGoldFlyAnimation(screenPoint, bonusConfigItem.Reward.Value);
-                }
-                else
-                {
-                    dispatcher.UIRequestAddCashFlyAnimation(screenPoint, 5 * (i + 1));
-                }
-            }
-        }
-
-        var reward = bonusConfig.GetDailyBonusRewardForDay(dailyBonusPopupViewModel.CurrentBonusDay);
-        if (reward.CashAmount > 0)
-        {
-            playerModel.AddCash(reward.CashAmount);
-        }
-        if (reward.GoldAmount > 0)
-        {
-            playerModel.AddGold(reward.GoldAmount);
-        }
-
-        playerModel.UpdateDailyBonus(dailyBonusPopupViewModel.OpenTimestamp, dailyBonusPopupViewModel.CurrentBonusDay);
-
-        await UniTask.Delay(2000);
-
         if (gameStateModel.ShowingPopupModel != null && gameStateModel.ShowingPopupModel.PopupType == PopupType.DailyBonus)
         {
-            gameStateModel.RemoveCurrentPopupIfNeeded();
+            var dailyBonusPopupViewModel = gameStateModel.ShowingPopupModel as DailyBonusPopupViewModel;
+            var lastBonusTakeTime = playerModel.BonusState.LastBonusTakeTimestamp;
+            if (DateTimeHelper.IsSameDays(lastBonusTakeTime, dailyBonusPopupViewModel.OpenTimestamp) == false)
+            {
+                var bonusConfigItems = bonusConfig.DailyBonusConfig;
+                for (var i = 0; i < bonusConfigItems.Length; i++)
+                {
+                    var bonusConfigItem = bonusConfigItems[i];
+                    if (dailyBonusPopupViewModel.CurrentBonusDay >= bonusConfigItem.DayNum)
+                    {
+                        var screenPoint = screenCalculator.WorldToScreenPoint(itemsWorldPositions[i]);
+                        if (bonusConfigItem.Reward.IsGold)
+                        {
+                            dispatcher.UIRequestAddGoldFlyAnimation(screenPoint, bonusConfigItem.Reward.Value);
+                        }
+                        else
+                        {
+                            dispatcher.UIRequestAddCashFlyAnimation(screenPoint, 5 * (i + 1));
+                        }
+                    }
+                }
+
+                var reward = bonusConfig.GetDailyBonusRewardForDay(dailyBonusPopupViewModel.CurrentBonusDay);
+                if (reward.CashAmount > 0)
+                {
+                    playerModel.AddCash(reward.CashAmount);
+                }
+                if (reward.GoldAmount > 0)
+                {
+                    playerModel.AddGold(reward.GoldAmount);
+                }
+
+                playerModel.UpdateDailyBonus(dailyBonusPopupViewModel.OpenTimestamp, dailyBonusPopupViewModel.CurrentBonusDay);
+
+                await UniTask.Delay(2000);
+
+                gameStateModel.RemoveCurrentPopupIfNeeded();
+            }
         }
     }
 }

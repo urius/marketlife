2909c03af448c3665775724aab41b9453953842a
Urius <isup89@rambler.ru>
2021-09-26 16:29:17 +0300

    add try\catch on load compensation data

--- Assets/Src/Commands/LoadSave/LoadCompensationDataCommand.cs
+++ Assets/Src/Commands/LoadSave/LoadCompensationDataCommand.cs
@@ -1,14 +1,17 @@
+using System;
 using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
+using UnityEngine;
 
 public struct LoadCompensationDataCommand
 {
     public async UniTask<bool> ExecuteAsync()
     {
-        var playerModel = PlayerModelHolder.Instance.UserModel;
+        var playerModelHolder = PlayerModelHolder.Instance;
+        var playerModel = playerModelHolder.UserModel;
         if (playerModel.BonusState.IsOldGameBonusProcessed == false)
         {
-            var url = string.Format(URLsHolder.Instance.GetFriendDataOldURL, playerModel.Uid);
+            var url = string.Format(URLsHolder.Instance.GetFriendDataOldURL, playerModelHolder.Uid);
             var resultOperation = await new WebRequestsSender().GetAsync(url);
             if (resultOperation.IsSuccess)
             {
@@ -18,17 +21,25 @@ public struct LoadCompensationDataCommand
                     if (responseCode == 1)
                     {
                         var dataImporter = DataImporter.Instance;
-                        var deserializedDataOld = JsonConvert.DeserializeObject<GetDataOldResponseDto>(resultOperation.Result);
-                        var oldGameUserModel = dataImporter.ImportOld(deserializedDataOld);
-                        var oldLevel = oldGameUserModel.ProgressModel.Level;
-                        var shopSquare = oldGameUserModel.ShopModel.ShopDesign.SizeX * oldGameUserModel.ShopModel.ShopDesign.SizeY;
-
-                        var compensationHolder = OldGameCompensationHolder.Instance;
-                        compensationHolder.SetupCompensation(new OldGameCompensation()
+                        try
                         {
-                            AmountCash = deserializedDataOld.data.cash_b + shopSquare * 100,
-                            AmountGold = deserializedDataOld.data.gold_b + oldLevel,
-                        });
+                            var deserializedDataOld = JsonConvert.DeserializeObject<GetDataOldResponseDto>(resultOperation.Result);
+                            var oldGameUserModel = dataImporter.ImportOld(deserializedDataOld);
+                            var oldLevel = oldGameUserModel.ProgressModel.Level;
+                            var shopSquare = oldGameUserModel.ShopModel.ShopDesign.SizeX * oldGameUserModel.ShopModel.ShopDesign.SizeY;
+
+                            var compensationHolder = OldGameCompensationHolder.Instance;
+                            compensationHolder.SetupCompensation(new OldGameCompensation()
+                            {
+                                AmountCash = deserializedDataOld.data.cash_b + shopSquare * 100,
+                                AmountGold = deserializedDataOld.data.gold_b + oldLevel,
+                            });
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogWarning($"Exception while parsing compensation data: {e.ToString()}\n slipping...");
+                            playerModel.MarkOldGameBonusProcessed();
+                        }
                     }
                     else if (responseCode == -1)
                     {
